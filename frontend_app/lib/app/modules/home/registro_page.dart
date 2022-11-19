4b10335e5d39b496601a656bdc1337556c51beca
Luiz Neto <luizcsneto@outlook.com>
2022-11-19 14:41:03 -0300

    Melhoria na interface de registro

--- frontend_app/lib/app/modules/home/registro_page.dart
+++ frontend_app/lib/app/modules/home/registro_page.dart
@@ -12,12 +12,11 @@ class RegistroStateful extends StatefulWidget {
 }
 
 class _RegistroStatefulState extends State<RegistroStateful> {
-
-  TextEditingController nomeController =  TextEditingController();
-  TextEditingController cpfController =  TextEditingController();
+  TextEditingController nomeController = TextEditingController();
+  TextEditingController cpfController = TextEditingController();
   TextEditingController telefoneController = TextEditingController();
-  TextEditingController emailController =  TextEditingController();
-  TextEditingController senhaController =  TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController senhaController = TextEditingController();
 
   RegistroRepository registroRepository = RegistroRepository();
   Usuario usuarioCadastro = Usuario();
@@ -41,172 +40,189 @@ class _RegistroStatefulState extends State<RegistroStateful> {
             ),
           ),
           Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                    width: 200,
-                    child: TextField(
-                      controller: nomeController,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold
-                      ),
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        hintText: "Nome*",
-                      ),
-                    ),
-                  ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 10
-                  )
+            child: Column(children: [
+              Padding(padding: EdgeInsets.only(top: 55)),
+              Text(
+                "REGISTRE-SE",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
                 ),
-                    SizedBox(
-                    width: 200,
-                    child: TextField(
-                      controller: cpfController,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold
-                      ),
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        hintText: "Cpf*",
-                      ),
-                    ),
-                  ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 10
-                  )
+              ),
+              Text(
+                "Crie sua conta. É de graça.",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
                 ),
-                SizedBox(
-                    width: 200,
-                    child: TextField(
-                      controller: telefoneController,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold
-                      ),
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        hintText: "Telefone",
-                      ),
+              ),
+            ]),
+          ),
+          Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 200,
+                height: 40,
+                child: TextField(
+                  controller: nomeController,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(),
+                    hintText: "Nome*",
+                    contentPadding: EdgeInsets.all(10),
+                  ),
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(top: 10)),
+              SizedBox(
+                width: 200,
+                height: 40,
+                child: TextField(
+                  controller: cpfController,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(),
+                    hintText: "Cpf*",
+                    contentPadding: EdgeInsets.all(10),
+                  ),
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(top: 10)),
+              SizedBox(
+                width: 200,
+                height: 40,
+                child: TextField(
+                  controller: telefoneController,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(),
+                    hintText: "Telefone",
+                    contentPadding: EdgeInsets.all(10),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+              ),
+              SizedBox(
+                width: 200,
+                height: 40,
+                child: TextField(
+                  controller: emailController,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(),
+                    hintText: "Email*",
+                    contentPadding: EdgeInsets.all(10),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+              ),
+              SizedBox(
+                width: 200,
+                height: 40,
+                child: TextField(
+                  controller: senhaController,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(),
+                    hintText: "Senha*",
+                    contentPadding: EdgeInsets.all(10),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+              ),
+              OutlinedButton(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(40, 12, 40, 12),
+                  child: Text(
+                    "REGISTRAR",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      top: 10
-                    ),
-                  ),
-                  SizedBox(
-                    width: 200,
-                    child: TextField(
-                      controller: emailController,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold
-                      ),
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        hintText: "Email*",
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      top: 10
-                    ),
-                  ),
-                  SizedBox(
-                    width: 200,
-                    child: TextField(
-                      controller: senhaController,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold
-                      ),
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        hintText: "Senha*",
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      top: 10
-                    ),
-                  ),
-                  FloatingActionButton(
-                        child: Icon(Icons.send_rounded),
-                        backgroundColor: Colors.blueGrey,  
-                        foregroundColor: Colors.white,  
-                        onPressed: () {
-                          if(nomeController.text.isNotEmpty){
-                            if(cpfController.text.isNotEmpty){
-                              if(emailController.text.isNotEmpty){
-                                if(senhaController.text.isNotEmpty){
-
-                                  usuarioCadastro.setNome(nomeController.text);
-                                  usuarioCadastro.setCpf(cpfController.text);
-                                  usuarioCadastro.setTelefone(telefoneController.text);
-                                  usuarioCadastro.setEmail(emailController.text);
-                                  usuarioCadastro.setSenha(senhaController.text);
-                                  registroRepository.registro(usuarioCadastro);
-                                  
-                                  Future.delayed(Duration(milliseconds: 1000),(){
-                                    Alert(
-                                      context: context, 
-                                      title: "PARABÉNS", 
-                                      desc: "O usuário ${usuarioCadastro.getNome()} foi cadastrado com sucesso. " 
-                                      ).show();
-                                  });
-                                
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(builder: (context) => const LoginStateful())
-                                    );
-                                }
-                              }
-                            }
-                          }
-                        }, 
-                      ),
-              ],
-            )
-          )
+                ),
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey,
+                  shape: StadiumBorder(),
+                ),
+                onPressed: () {
+                  verificarDadosCadastro();
+                },
+              )
+            ],
+          ))
         ],
       ),
       bottomNavigationBar: BottomAppBar(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.blueAccent
-          ),
-          child: Row(
+          child: Container(
+        decoration: BoxDecoration(color: Colors.blueAccent),
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
               "@copyright",
               style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 15
-              ),
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15),
             ),
             Padding(
               padding: EdgeInsets.all(12),
             )
           ],
         ),
-        )
-      ),
+      )),
     );
+  }
+
+  void verificarDadosCadastro() {
+    if (nomeController.text.isNotEmpty) {
+      if (cpfController.text.isNotEmpty) {
+        if (emailController.text.isNotEmpty) {
+          if (senhaController.text.isNotEmpty) {
+            usuarioCadastro.setNome(nomeController.text);
+            usuarioCadastro.setCpf(cpfController.text);
+            usuarioCadastro.setTelefone(telefoneController.text);
+            usuarioCadastro.setEmail(emailController.text);
+            usuarioCadastro.setSenha(senhaController.text);
+
+            registroRepository.registro(usuarioCadastro);
+
+            Future.delayed(Duration(milliseconds: 1000), () {
+              Alert(
+                      context: context,
+                      title: "PARABÉNS",
+                      desc:
+                          "O usuário ${usuarioCadastro.getNome()} foi cadastrado com sucesso. ")
+                  .show();
+            });
+
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const LoginStateful()));
+          }
+        }
+      }
+    }
   }
 }
